867056c8d11abec30785a6739005db6254f39bba
Jari Aalto <jari.aalto@cante.net>
2012-01-30 15:13:46 +0200

    3.1-1

---
+++ install-after.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+#
+# install-after.sh -- Custom installation
+#
+# The script will receive one argument: relative path to
+# installation root directory. Script is called like:
+#
+#    $ install-after.sh .inst
+#
+# This script is run after [install] command.
+
+Cmd()
+{
+    echo "$@"
+    [ "$test" ] && return
+    "$@"
+}
+
+Main()
+{
+    root=${1:-".inst"}
+
+    if [ "$root"  ] && [ -d $root ]; then
+
+        root=$(echo $root | sed 's,/$,,')  # Delete trailing slash
+
+        docdir=$(cd $root/usr/share/doc/jlint-* && pwd)
+
+        [ ! "$docdir" ] && return 1
+
+        PATH=".:$PATH"
+
+        Cmd install -m 644 *.texi jlint.sh $docdir/
+        Cmd cd $docdir/
+        Cmd texi2html *.texi
+
+        echo "Remove unneeded file (texi: @include jlint.sh)"
+        Cmd rm -f jlint.sh
+    fi
+}
+
+Main "$@"
+
+# End of file
